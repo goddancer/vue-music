823c7247800710ac91e94c83c4a69fb87d0636f0
goddancer <goddancer@foxmail.com>
2018-05-09 17:41:11 +0800

    vue-music demo

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'//fastclick引入方法
+import VueLazyload from 'vue-lazyload'
+import store from './store'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body);//fastclick使用方法，建议绑定在body上
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+});
